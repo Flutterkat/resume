0e06d118964b617b93f9f8598776b28cb5a945f7
Flutterkat <85975055+flutterkat@users.noreply.github.com>
2021-08-30 11:50:15 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,13 +2,14 @@ const pages = document.getElementsByClassName('badge');
 const p = document.getElementsByClassName('description');
 const cube = document.getElementById('photo-cube');
 const cubeContainer = document.getElementsByClassName('cube-container');
+const cubeFocus = document.getElementsByTagName('a');
 let RNG = function () {
     return Math.floor(Math.random() * (180 - (-180)) + (-180));
   };
   
 
  function rotator() {
-    setInterval (function () {if (p[0].style.display === 'none' && p[1].style.display === 'none' && p[2].style.display === 'none' && p[3].style.display === 'none' && p[4].style.display === 'none' && p[5].style.display === 'none') {
+    setInterval (function () {if (p[0].style.display === 'none' && p[1].style.display === 'none' && p[2].style.display === 'none' && p[3].style.display === 'none' && p[4].style.display === 'none' && p[5].style.display === 'none' && cubeFocus[0] !== document.activeElement && cubeFocus[1] !== document.activeElement && cubeFocus[2] !== document.activeElement && cubeFocus[3] !== document.activeElement && cubeFocus[4] !== document.activeElement && cubeFocus[5] !== document.activeElement) {
         cube.style.transition = 'transform 20s linear';
         cube.style.transform = `rotateX(${RNG()}deg) rotateY(${RNG()}deg) rotateZ(${RNG()}deg)`;
     } else {
@@ -46,6 +47,13 @@ pages[0].addEventListener('click', ()=>{
     }
 });
 
+cubeFocus[0].addEventListener('focus', ()=>{
+    cube.style.transition = 'transform 1s ease-in-out';
+    cube.style.transform = 'rotateX(0deg)';
+    cubeContainer[0].style.marginLeft = '';
+    cubeContainer[0].style.marginRight = '';
+    });
+
 pages[1].addEventListener('click', ()=>{
     cube.style.transition = 'transform 1s ease-in-out';
     cube.style.transform = 'rotateX(90deg)';
@@ -63,6 +71,13 @@ pages[1].addEventListener('click', ()=>{
         startRotator();
     }
 });
+
+cubeFocus[1].addEventListener('focus', ()=>{
+    cube.style.transition = 'transform 1s ease-in-out';
+    cube.style.transform = 'rotateX(90deg)';
+    cubeContainer[0].style.marginLeft = '';
+    cubeContainer[0].style.marginRight = '';
+    });
 
 pages[2].addEventListener('click', ()=>{
     cube.style.transition = 'transform 1s ease-in-out';
@@ -83,6 +98,13 @@ pages[2].addEventListener('click', ()=>{
     }
 });
 
+cubeFocus[2].addEventListener('focus', ()=>{
+    cube.style.transition = 'transform 1s ease-in-out';
+    cube.style.transform = 'rotateY(90deg)';
+    cubeContainer[0].style.marginLeft = '50px';
+    cubeContainer[0].style.marginRight = '';
+    });
+
 pages[3].addEventListener('click', ()=>{
     cube.style.transition = 'transform 1s ease-in-out';
     cube.style.transform = 'rotateY(-90deg)';
@@ -99,6 +121,13 @@ pages[3].addEventListener('click', ()=>{
         p[3].style.display = 'none';
         startRotator();
     }
+});
+
+cubeFocus[3].addEventListener('focus', ()=>{
+    cube.style.transition = 'transform 1s ease-in-out';
+    cube.style.transform = 'rotateY(-90deg)';
+    cubeContainer[0].style.marginLeft = '';
+    cubeContainer[0].style.marginRight = '50px';
 });
 
 pages[4].addEventListener('click', ()=>{
@@ -119,6 +148,13 @@ pages[4].addEventListener('click', ()=>{
     }
 });
 
+cubeFocus[4].addEventListener('focus', ()=>{
+    cube.style.transition = 'transform 1s ease-in-out';
+    cube.style.transform = 'rotateY(180deg)';
+    cubeContainer[0].style.marginLeft = '';
+    cubeContainer[0].style.marginRight = '';
+});
+
 pages[5].addEventListener('click', ()=>{
     cube.style.transition = 'transform 1s ease-in-out';
     cube.style.transform = 'rotateX(-90deg)';
@@ -135,4 +171,11 @@ pages[5].addEventListener('click', ()=>{
         p[5].style.display = 'none';
         startRotator();
     }
+});
+
+cubeFocus[5].addEventListener('focus', ()=>{
+    cube.style.transition = 'transform 1s ease-in-out';
+    cube.style.transform = 'rotateX(-90deg)';
+    cubeContainer[0].style.marginLeft = '';
+    cubeContainer[0].style.marginRight = '';
 });
